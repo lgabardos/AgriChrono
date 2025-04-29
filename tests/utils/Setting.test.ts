a8004f2e4507937dbb053358ef70abfecf814727
Laurent Gabardos <l.gabardos@proton.me>
2025-04-29 15:33:31 +0200

    come one

--- tests/utils/Setting.test.ts
+++ tests/utils/Setting.test.ts
@@ -4,4 +4,7 @@ import Setting from '../../src/utils/Setting'
 test('Init', () => {
   const withoutAssignments = new Setting([], [], [], [])
   expect(withoutAssignments.assignments).toBeUndefined()
+
+  const withAssignments = new Setting([], [], [], [], [])
+  expect(withAssignments.assignments).not.toBeUndefined()
 })
